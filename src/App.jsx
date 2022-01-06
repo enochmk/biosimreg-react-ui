3b62747fd7181d38504e240ba8bcdd78ad7cabea
Enoch Klufio <enochmk94@gmail.com>
2022-01-06 01:30:29 +0000

    Init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+	return (
+		<div className='container p-8'>
+			<h2 className='text-3xl'>BioSimReg App</h2>
+			<button className='btn btn-primary btn-lg'>New Button</button>
+		</div>
+	);
+}
+
+export default App;
