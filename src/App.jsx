6c5e351f2a05d61471d76a7254e7351272fde627
Enoch Klufio <enochmk94@gmail.com>
2022-02-05 14:54:29 +0000

    chore(img): add new image assests

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Admin from './layouts/Admin';
 import Auth from './layouts/Auth';
 import NotFound from './views/notFound/NotFound';
+
+import Index from './views/others/Index';
 
 function App() {
   return (
     <Routes>
       <Route path="/auth/*" element={<Auth />} />
       <Route path="/admin/*" element={<Admin />} />
+      <Route path="/" element={<Index />} />
       <Route path="/notfound" element={<NotFound />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="*" element={<Navigate from="*" to="/" />} />
     </Routes>
   );
 }
---
+++ src/views/others/Index.jsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <h2 className="text-3xl">Home Page</h2>
+      </div>
+    </div>
+  );
+}
