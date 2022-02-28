62fffb922f560314e0d3fc20fb3404ebae254ba8
Enoch Klufio <enochmk94@gmail.com>
2022-02-28 16:55:34 +0000

    refactor: update Headerstats to get data from backend

--- src/components/Headers/HeaderStats.jsx
+++ src/components/Headers/HeaderStats.jsx
@@ -1,70 +1,75 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { updateStats } from '../../features/stats/statsSlice';
+import useAxiosPrivate from '../../hooks/useAxiosPrivate';
 import CardStats from '../Cards/CardStats';
 
-export default function HeaderStats({ data }) {
+const HeaderStats = () => {
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.statistics.data);
+  const axios = useAxiosPrivate();
+
+  useEffect(() => {
+    axios.get('/profile/stats').then((response) => {
+      dispatch(updateStats(response.data));
+    });
+  }, [dispatch, axios]);
+
   return (
-    <>
-      <div className="relative bg-blue-500 md:pt-32 pb-32 pt-12 w-full">
-        <div className="px-4 md:px-10 mx-auto w-full">
-          <div>
-            <div className="flex flex-wrap">
-              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
-                <CardStats
-                  statSubtitle="Total Linking"
-                  statTitle={
-                    data?.linking?.total_linking_count.toString() || '0'
-                  }
-                  statArrow="up"
-                  statPercent=""
-                  statPercentColor="text-emerald-500"
-                  statDescripiron="Since launch"
-                  statIconName="far fa-chart-bar"
-                  statIconColor="bg-red-500"
-                />
-              </div>
-              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
-                <CardStats
-                  statSubtitle="Total BCAP"
-                  statTitle={data?.linking?.total_bcap_count.toString() || '0'}
-                  statArrow="up"
-                  statPercent=""
-                  statPercentColor="text-red-500"
-                  statDescripiron="Since launch"
-                  statIconName="fas fa-chart-pie"
-                  statIconColor="bg-orange-500"
-                />
-              </div>
-              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
-                <CardStats
-                  statSubtitle="Daily Linking"
-                  statTitle={
-                    data?.linking?.daily_linking_count.toString() || '0'
-                  }
-                  statArrow="up"
-                  statPercent=""
-                  statPercentColor="text-orange-500"
-                  statDescripiron="Today"
-                  statIconName="fas fa-users"
-                  statIconColor="bg-pink-500"
-                />
-              </div>
-              <div className="w-full lg:w-6/12 xl:w-3/12 px-4">
-                <CardStats
-                  statSubtitle="Daily BCAP"
-                  statTitle={data?.linking?.daily_bcap_count.toString() || '0'}
-                  statArrow="up"
-                  statPercent=""
-                  statPercentColor="text-emerald-500"
-                  statDescripiron="today"
-                  statIconName="fas fa-percent"
-                  statIconColor="bg-blue-500"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    <section className="relative bg-blue-500 md:pt-32 pb-32 pt-12 w-full">
+      <main className="px-4 md:px-10 mx-auto w-full flex flex-wrap">
+        <article className="w-full lg:w-6/12 xl:w-3/12 px-4">
+          <CardStats
+            statSubtitle="Total Linking"
+            statTitle={data?.linking?.total_linking_count.toString() || '0'}
+            statArrow="up"
+            statPercent=""
+            statPercentColor="text-emerald-500"
+            statDescripiron="Since launch"
+            statIconName="far fa-chart-bar"
+            statIconColor="bg-red-500"
+          />
+        </article>
+        <article className="w-full lg:w-6/12 xl:w-3/12 px-4">
+          <CardStats
+            statSubtitle="Total BCAP"
+            statTitle={data?.linking?.total_bcap_count.toString() || '0'}
+            statArrow="up"
+            statPercent=""
+            statPercentColor="text-red-500"
+            statDescripiron="Since launch"
+            statIconName="fas fa-chart-pie"
+            statIconColor="bg-orange-500"
+          />
+        </article>
+        <article className="w-full lg:w-6/12 xl:w-3/12 px-4">
+          <CardStats
+            statSubtitle="Daily Linking"
+            statTitle={data?.linking?.daily_linking_count.toString() || '0'}
+            statArrow="up"
+            statPercent=""
+            statPercentColor="text-orange-500"
+            statDescripiron="Today"
+            statIconName="fas fa-users"
+            statIconColor="bg-pink-500"
+          />
+        </article>
+        <article className="w-full lg:w-6/12 xl:w-3/12 px-4">
+          <CardStats
+            statSubtitle="Daily BCAP"
+            statTitle={data?.linking?.daily_bcap_count.toString() || '0'}
+            statArrow="up"
+            statPercent=""
+            statPercentColor="text-emerald-500"
+            statDescripiron="today"
+            statIconName="fas fa-percent"
+            statIconColor="bg-blue-500"
+          />
+        </article>
+      </main>
+    </section>
   );
-}
+};
+
+export default HeaderStats;
