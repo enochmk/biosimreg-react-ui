06b0362e2e56aad53da814af535c13a51098f43e
Enoch Klufio <enochmk94@gmail.com>
2022-02-28 16:25:05 +0000

    refactor: clean up dashboard

--- src/views/admin/Dashboard.jsx
+++ src/views/admin/Dashboard.jsx
@@ -1,18 +1,14 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import CardLineChart from '../../components/Cards/CardLineChart';
 import CardBarChart from '../../components/Cards/CardBarChart';
 import CardPageVisits from '../../components/Cards/CardPageVisits';
 import CardSocialTraffic from '../../components/Cards/CardSocialTraffic';
 import { changeTitle } from '../../features/navbar/navbarSlice';
-import useRefreshToken from '../../hooks/useRefreshToken';
-import { getStatistics } from '../../features/stats/statsSlice';
 
 function Dashboard() {
   const dispatch = useDispatch();
-  const accessToken = useSelector((state) => state.auth.accessToken);
-  const refreshToken = useRefreshToken();
 
   useEffect(() => {
     dispatch(changeTitle('Dashboard'));
@@ -21,18 +17,6 @@ function Dashboard() {
 
   return (
     <>
-      <button className="btn btn-info m-3" onClick={() => refreshToken()}>
-        Refresh Token
-      </button>
-
-      <button
-        className="btn btn-primary m-3"
-        onClick={() => {
-          dispatch(getStatistics(accessToken));
-        }}
-      >
-        Update Statistics
-      </button>
       <div className="flex flex-wrap">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
           <CardLineChart />
