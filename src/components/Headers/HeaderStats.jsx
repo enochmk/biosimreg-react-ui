2eca91ccfa21c99b0d1233cd8107f80ff81da416
Enoch Klufio <enochmk94@gmail.com>
2022-03-28 12:36:16 +0000

    fix: :bug: headerstats value were NULL

--- src/components/Headers/HeaderStats.jsx
+++ src/components/Headers/HeaderStats.jsx
@@ -7,7 +7,7 @@ import CardStats from '../Cards/CardStats';
 
 const HeaderStats = () => {
   const dispatch = useDispatch();
-  const data = useSelector((state) => state.statistics.data);
+  const stats = useSelector((state) => state.statistics.data);
   const axios = useAxiosPrivate();
 
   useEffect(() => {
@@ -16,20 +16,13 @@ const HeaderStats = () => {
     });
   }, [dispatch, axios]);
 
-  const {
-    dailyBcapCount,
-    dailyLinkingCount,
-    totalBcapCount,
-    totalLinkingCount,
-  } = data;
-
   return (
     <section className="relative bg-blue-500 md:pt-32 pb-32 pt-12 w-full">
       <main className="px-4 md:px-10 mx-auto w-full flex flex-wrap">
         <article className="w-full lg:w-6/12 xl:w-3/12 px-4">
           <CardStats
             statSubtitle="Total Linking"
-            statTitle={totalLinkingCount.toString() || 'N/A'}
+            statTitle={stats?.totalLinkingCount?.toString() || 'N/A'}
             statArrow="up"
             statPercent=""
             statPercentColor="text-emerald-500"
@@ -41,7 +34,7 @@ const HeaderStats = () => {
         <article className="w-full lg:w-6/12 xl:w-3/12 px-4">
           <CardStats
             statSubtitle="Total BCAP"
-            statTitle={totalBcapCount.toString() || 'N/A'}
+            statTitle={stats?.totalBcapCount?.toString() || 'N/A'}
             statArrow="up"
             statPercent=""
             statPercentColor="text-red-500"
@@ -53,7 +46,7 @@ const HeaderStats = () => {
         <article className="w-full lg:w-6/12 xl:w-3/12 px-4">
           <CardStats
             statSubtitle="Daily Linking"
-            statTitle={dailyLinkingCount.toString() || 'N/A'}
+            statTitle={stats?.dailyLinkingCount?.toString() || 'N/A'}
             statArrow="up"
             statPercent=""
             statPercentColor="text-orange-500"
@@ -65,7 +58,7 @@ const HeaderStats = () => {
         <article className="w-full lg:w-6/12 xl:w-3/12 px-4">
           <CardStats
             statSubtitle="Daily BCAP"
-            statTitle={dailyBcapCount.toString() || 'N/A'}
+            statTitle={stats?.dailyBcapCount?.toString() || 'N/A'}
             statArrow="up"
             statPercent=""
             statPercentColor="text-emerald-500"
