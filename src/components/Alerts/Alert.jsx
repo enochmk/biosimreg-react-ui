ea65d09e0c7adb65dd81ad84fe2cbe411243999f
Enoch Klufio <enochmk94@gmail.com>
2022-03-28 02:23:54 +0000

    fix: :bug: consistency show alert color

--- src/components/Alerts/Alert.jsx
+++ src/components/Alerts/Alert.jsx
@@ -4,8 +4,10 @@ import SuccessIcon from './SuccessIcon';
 const Alert = ({ show, title, message, status }) => {
   if (!show) return null;
 
+  const alertStatus = status === 'success' ? 'alert-success' : 'alert-error';
+
   return (
-    <main className={`rounded-none my-2 alert shadow-lg alert-${status}`}>
+    <main className={`rounded-none my-2 shadow-lg alert ${alertStatus}`}>
       <div className="space-x-1 items-center">
         {status === 'success' ? (
           <SuccessIcon className="w-10 h-10" />
